c8de124b70510d261aef9f7c453f7a82fdc902db
sayan4545 <sayanchatterjee3010@gmail.com>
2025-07-22 03:06:50 +0530

    Added validations

--- src/main/java/com/dev/springboot/bootcamp01/springbootmvc/dtos/EmployeeDto.java
+++ src/main/java/com/dev/springboot/bootcamp01/springbootmvc/dtos/EmployeeDto.java
@@ -2,6 +2,7 @@ package com.dev.springboot.bootcamp01.springbootmvc.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.validation.annotation.Validated;
 
@@ -16,7 +17,8 @@ import java.time.LocalDate;
 public class EmployeeDto {
     private Long id;
 
-    @NotNull(message = "Required field in field name")
+    @NotBlank(message = "Required field in field name")
+    @Size(min = 1,max = 20,message = "range (1,21)")
     private String name;
     private Integer age;
     private LocalDate doj;
--- src/main/java/com/dev/springboot/bootcamp01/springbootmvc/entities/EmployeeEntity.java
+++ src/main/java/com/dev/springboot/bootcamp01/springbootmvc/entities/EmployeeEntity.java
@@ -1,10 +1,7 @@
 package com.dev.springboot.bootcamp01.springbootmvc.entities;
-
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.time.LocalDate;
 
 @Entity
